88a287e9ae5874f06462bd86922619a94f4dc45d
Dinesh <dinu1763@gmail.com>
2021-12-26 11:46:40 +0530

    update

---
+++ react-intro/src/App.js
@@ -0,0 +1,15 @@
+  
+import logo from './logo.svg';
+import './App.css';
+import StaticCard from './components/StaticCard'
+
+function App() {
+  return (
+    <div className="App">
+      <StaticCard heading={"Mobile Operating System"} list={['Android', 'Blackberry', 'iPhone', 'Windows Phone']} />
+      <StaticCard heading={"Mobile Manufacturers"} list={['Samsung', 'Nokia', 'Micromax', 'Apple']} />
+    </div>
+  );
+}
+
+export default App;
